31942a95ea38ff0bf0c1a1fd9adc7d93ae11dd18
daaroni <aaronsilvennoinen@gmail.com>
2024-09-01 22:53:08 +0300

    Testaus

--- Testaus.js
+++ Testaus.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>expo test</Text>
+      <Text>expo testi</Text>
       <StatusBar style="auto" />
     </View>
   );
